ed4dad6055549ef9a8c5cb1b0096e47d48fb9ae7
johnkevindizon <118444998+johnkevindizon@users.noreply.github.com>
2023-05-28 08:38:05 +0800

    May 28 2023
    change look

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 let toggleNavStatus = false;
 
 let toggleNav = function () {
@@ -16,3 +14,54 @@ let toggleNav = function () {
       toggleNavStatus = false;
    }
 }
+
+let toggleABOUTStatus = false;
+
+let toggleABOUT = function () {
+   let getABOUT = document.querySelector(".about-sidebar");
+
+   if (toggleABOUTStatus === false) {
+      getABOUT.style.display = "block";
+      toggleABOUTStatus = true;
+   } else {
+      getABOUT.style.display = "none";
+      toggleABOUTStatus = false;
+   }
+}
+
+
+let toggleSRBMStatus = false;
+
+let toggleSRBM = function () {
+   let getSRBM = document.querySelector(".srbm-sidebar");
+
+   if (toggleSRBMStatus === false) {
+      getSRBM.style.display = "block";
+      toggleSRBMStatus = true;
+   } else {
+      getSRBM.style.display = "none";
+      toggleSRBMStatus = false;
+   }
+}
+
+
+let toggleGUITARStatus = false;
+
+let toggleGUITAR = function () {
+   let getGUITAR = document.querySelector(".guitar-sidebar");
+
+   if (toggleGUITARStatus === false) {
+      getGUITAR.style.display = "block";
+      toggleGUITARStatus = true;
+   } else {
+      getGUITAR.style.display = "none";
+      toggleGUITARStatus = false;
+   }
+}
+
+
+// $(document).ready(function(){
+//    $('.sub-btn').click(function(){
+//       $(this).next('.sub-menu').slideToggle();
+//    });
+// });
